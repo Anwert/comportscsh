86ea4f7c28e38da96cd25ec880ae9e788c0e9151
Anwert <fternftern@gmail.com>
2018-11-15 03:09:39 +0500

    Добавьте файлы проекта.

---
+++ comportswinforms/ComPortsService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+using System.IO.Ports;
+using System.Management;
+using System.Collections.Generic;
+
+namespace comportswinforms
+{
+	class ComPortsService
+	{
+		static SerialPort _serialPort;
+		Thread _readThread;
+		static bool cont { get; set; }
+		static string message { get; set; }
+
+		public delegate void AppendText(string msg);
+		static AppendText appendText;
+
+        public ComPortsService(AppendText append_text)
+        {
+			appendText = append_text;
+			message = "";
+			cont = true;
+
+			// Создать объект класса SerialPort с настройками по умолчанию
+			_serialPort = new SerialPort();
+
+			// Указать таймауты
+			_serialPort.ReadTimeout = 1;
+			_serialPort.WriteTimeout = 1;
+
+			_readThread = new Thread(Read);
+			_readThread.Start();
+		}
+
+		public void SelectPortAndStart(string port_name)
+		{
+			if (_serialPort.IsOpen)
+				_serialPort.Close();
+
+			_serialPort.PortName = port_name;
+			_serialPort.Open();
+		}
+
+		public void SendMessage(string message_to_send)
+		{
+			try
+			{
+				_serialPort.WriteLine(
+					String.Format("<{0}>: {1}", _serialPort.PortName, message_to_send));
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		public static void Read()
+		{
+			while (cont)
+			{
+				try
+				{
+					appendText(_serialPort.ReadLine());
+				}
+				catch
+				{
+					try
+					{
+						if (_serialPort.IsOpen)
+							_serialPort.WriteLine("");
+					}
+					catch { }
+				}
+			}
+		}
+
+		public void Close()
+		{
+			cont = false;
+			_readThread.Join();
+			_serialPort.Close();
+		}
+
+		public IEnumerable<string> GetPortsNames()
+		{
+			ManagementObjectSearcher searcher =
+				new ManagementObjectSearcher("root\\WMI",
+					"SELECT * FROM MSSerial_PortName");
+
+			foreach (ManagementObject queryObj in searcher.Get())
+				yield return queryObj["PortName"].ToString();
+		}
+	}
+}
---
+++ comportswinforms/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace comportswinforms
+{
+	public partial class Form1 : Form
+	{
+		ComPortsService _comPortsService;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			_comPortsService = new ComPortsService(AppendText);
+			string[] ports_names = _comPortsService.GetPortsNames().ToArray<string>();
+			foreach (string port_name in ports_names)
+				comboBox1.Items.Add(port_name);
+		}
+
+		public void AppendText(string msg)
+		{
+			if (!string.IsNullOrEmpty(msg))
+				richTextBox1.AppendText(msg + Environment.NewLine);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string message_to_send = textBox1.Text;
+			try
+			{
+				_comPortsService.SendMessage(message_to_send);
+			}
+			catch { }
+			ShowMyMessage(message_to_send);
+		}
+
+		private void ShowMyMessage(string message_to_send)
+		{
+			richTextBox1.AppendText(String.Format("<Me>: {0}", message_to_send) +
+				Environment.NewLine);
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				string selected_item_str = (string)comboBox1.SelectedItem;
+				_comPortsService.SelectPortAndStart(selected_item_str);
+			}
+			catch
+			{
+				MessageBox.Show("This port is busy.");
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			_comPortsService.Close();
+		}
+	}
+}
